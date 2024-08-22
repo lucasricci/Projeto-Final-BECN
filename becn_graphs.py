68aa6c8be213a9e53da0d74326bca948d37328c4
Lucas Ricci <lr.lucasricci@gmail.com>
2024-08-22 14:05:10 -0300

    Banana

--- becn_graphs.py
+++ becn_graphs.py
@@ -3,21 +3,8 @@ import seaborn as sns
 import pandas as pd
 
 df = pd.read_csv("BECN.csv")
-
-# df.loc[df.banana == "Amanda", "grau"] = 1
-# df.loc[df.banana == "Otavia", "grau"] = 1
-# df.loc[df.banana == "Betania", "grau"] = 1
-
-
-# sns.lmplot(x="r", y="resistividade", data=df)
-# sns.lmplot(x="g", y="resistividade", data=df)
-# sns.lmplot(x="b", y="resistividade", data=df)
+maturity = ["#00FF00", "#7FFF00", "#BFFF00", "#DFFF00", "#FFFF00"]
 
 plt.figure(dpi=160)
-sns.lineplot(
-    x="tempo",
-    y="resistencia",
-    hue="grau",
-    data=df,
-)
+sns.lineplot(x="tempo", y="resistencia", hue="grau", data=df, palette="bright")
 plt.show()
